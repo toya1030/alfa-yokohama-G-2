75797225f54f604bc66c30ef0a3a54b653009cfa
toya1030 <89884245+toya1030@users.noreply.github.com>
2021-09-13 11:34:25 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
           q1: '大山',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'door',
           // q2: 'えええ',
           // q3: 'おおお'
         },
